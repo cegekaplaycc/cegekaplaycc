7285944ce8c9b25066dc0f0278eaf7caaf003ac4
Ben Verbeken <ben@ostia.be>
2012-01-12 23:51:31 +0100

    [MAIN] added cssgrid.net css + changed the default template

--- main/app/controllers/Application.java
+++ main/app/controllers/Application.java
@@ -7,9 +7,9 @@ import views.html.*;
 
 public class Application extends Controller {
   
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
+    public static Result grid() {
+        return ok(grid.render("129129"));
+    }
  
   
 }

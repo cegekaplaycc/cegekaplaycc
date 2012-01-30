c92d8676857b0ef4b1028cad7622cca4dd3346e3
mroloux <matti.roloux@gmail.com>
2012-01-30 16:38:50 +0100

    Always clear the persistence context in integration tests

--- test/models/PlayerIntegrationTest.java
+++ test/models/PlayerIntegrationTest.java
@@ -3,27 +3,26 @@ package models;
 import org.fest.assertions.Assertions;
 import org.junit.Test;
 
-import play.db.jpa.JPABase;
-
 public class PlayerIntegrationTest extends IntegrationTestCase {
 
 	@Test
 	public void canBePersisted() {
 		Player player = new Player("ID", "jos");
 		player.save();
-		
+
+		Player.em().clear();
 		Assertions.assertThat(Player.findAll()).hasSize(1);
 		Player refreshed = (Player) Player.findAll().iterator().next();
-		
+
 		Assertions.assertThat(refreshed.getUserId()).isEqualTo("ID");
 		Assertions.assertThat(refreshed.getName()).isEqualTo("jos");
 	}
-	
+
 	@Test
 	public void canHavePersistedHorses() {
 		Player player = new Player("ID", "jos");
 		player.save();
-		
+
 		Horse horse1 = new Horse("horse1");
 		Horse horse2 = new Horse("horse2");
 		horse1.save();
@@ -31,11 +30,11 @@ public class PlayerIntegrationTest extends IntegrationTestCase {
 
 		player.addHorse(horse1);
 		player.addHorse(horse2);
-		
-		
-		Player refreshedPlayer = player.refresh();
+
+		Player.em().clear();
+		Player refreshedPlayer = Player.findById(player.getId());
 		Assertions.assertThat(refreshedPlayer.getHorses()).hasSize(2);
 		Assertions.assertThat(refreshedPlayer.getId()).isEqualTo(player.getId());
-		
+
 	}
 }

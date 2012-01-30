7f87b40ff10a8aecb97a87e0938f01c624af8cfc
Ben Verbeken <ben@ostia.be>
2012-01-30 14:18:06 +0100

    moved securesocial to a separate controller

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,7 +4,7 @@ import controllers.securesocial.SecureSocial;
 import play.mvc.Controller;
 import play.mvc.With;
 
-@With( SecureSocial.class )
+
 public class Application extends Controller {
 
     public static void index() {

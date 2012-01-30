b54ced3850e1bbc0db69875403a685ff440b85b9
Wouter Groeneveld <jefklak@gmail.com>
2012-01-30 14:01:28 +0100

    bkwog: random horses! yay

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,15 @@
 package controllers;
 
-import controllers.securesocial.SecureSocial;
+import models.Horse;
 import play.mvc.Controller;
 import play.mvc.With;
+import controllers.securesocial.SecureSocial;
 
 @With( SecureSocial.class )
 public class Application extends Controller {
 
     public static void index() {
+    	renderArgs.put("horses", Horse.findAll());
         render();
     }
 

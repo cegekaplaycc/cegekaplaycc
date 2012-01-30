c9497f447e2e67db0762c1aa72caaba0917c028f
Wouter Groeneveld <jefklak@gmail.com>
2012-01-30 14:51:17 +0100

    bkwog: private maken horse stuff

--- app/models/Horse.java
+++ app/models/Horse.java
@@ -12,17 +12,13 @@ public class Horse extends Model {
 	private String name;
 	private long price;
 
-<<<<<<< HEAD
 	public Horse() {
 	}
 
-=======
->>>>>>> 9f926598a6db836b8fa4eec4c157231c3e71a389
 	public Horse(String name) {
 		this.name = name;
 	}
 
-<<<<<<< HEAD
 	public String getName() {
 		return name;
 	}
@@ -37,11 +33,8 @@ public class Horse extends Model {
 
 	public void setPrice(long price) {
 		this.price = price;
-=======
-	public Horse() {
->>>>>>> 9f926598a6db836b8fa4eec4c157231c3e71a389
 	}
-
+	
 	@Override
 	public String toString() {
 		return name;

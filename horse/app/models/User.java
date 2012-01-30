f2c042712871fbf5316faacf76cdade43ebfb204
Wouter Groeneveld <jefklak@gmail.com>
2012-01-30 13:23:04 +0100

    bkwog: integratietesten en user stuff

--- horse/app/models/User.java
+++ horse/app/models/User.java
@@ -1,6 +1,34 @@
 package models;
 
+import javax.persistence.Entity;
+
 import play.db.jpa.Model;
 
-public class User extends Model{
+@Entity
+public class User extends Model {
+
+	private String name;
+	private final String userId;
+
+	public User(String userId) {
+		this.userId = userId;
+	}
+
+	public User(String userId, String name) {
+		this(userId);
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
 }

aada0b486bcb9c1ea322ab3be32fa20cc27aca92
Mercer Traieste <mercer.traieste@gmail.com>
2012-01-30 15:55:04 +0200

    Added avatar with name, logout redirect.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,17 @@
 package controllers;
 
+import controllers.securesocial.SecureSocial;
 import models.Horse;
 import play.mvc.Controller;
-import play.mvc.With;
-import controllers.securesocial.SecureSocial;
-
 
 public class Application extends Controller {
-
     public static void index() {
-    	renderArgs.put("horses", Horse.findAll());
+        renderArgs.put("horses", Horse.findAll());
+        renderArgs.put("user", SecureSocial.getCurrentUser());
+        render();
+    }
+    
+    public static void logout() {
         render();
     }
 

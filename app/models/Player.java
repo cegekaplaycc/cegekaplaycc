52d41871033f922bf9abeff6a989081310d833db
Mercer Traieste <mercer.traieste@gmail.com>
2012-01-30 17:21:01 +0200

    Added a database implementation for userservice

--- app/models/Player.java
+++ app/models/Player.java
@@ -3,10 +3,14 @@ package models;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import play.data.validation.Required;
 import play.data.validation.Unique;
 import play.db.jpa.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Player extends Model {
@@ -15,7 +19,7 @@ public class Player extends Model {
 
     @Column(unique = true)
 	private final String userId;
-	
+
 	@OneToMany
 	public Set<Horse> horses = new HashSet<Horse>();
 
@@ -39,11 +43,11 @@ public class Player extends Model {
 	public String getUserId() {
 		return userId;
 	}
-	
+
 	public Set<Horse> getHorses() {
 		return horses;
 	}
-	
+
 	public void addHorse(Horse horse) {
 		this.horses.add(horse);
 		this.save();

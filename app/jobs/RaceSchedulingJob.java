17bcc47d3e76507c5f523eb745e5b742f65e90da
unknown <BKTID@HI22286.cegekanv.corp.local>
2012-03-09 14:17:17 +0100

    BKTID: STORY Race01: added page to manually trigger jobs

--- app/jobs/RaceSchedulingJob.java
+++ app/jobs/RaceSchedulingJob.java
@@ -5,12 +5,14 @@ import models.Race;
 import play.db.jpa.JPABase;
 import play.jobs.Every;
 import play.jobs.Job;
+import play.jobs.On;
 
-@Every("15min")
+@On("0 0 0 * * ?")
 public class RaceSchedulingJob extends Job {
 
 	@Override
 	public void doJob() throws Exception {
 		new Race().save();
 	}
+	
 }

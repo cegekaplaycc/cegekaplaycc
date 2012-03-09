74b529aa613ea84210b38c1f565b8e92c1dededf
Kristof Lemmens <kristof.lemmens@cegeka.be>
2012-03-09 18:45:30 +0100

    kristof: cleanup old LoginController, cleanup new views twitter bootstrap, fix username at root page after login

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,10 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.With;
+import controllers.securesocial.SecureSocialPublic;
 
+@With(SecureSocialPublic.class)
 public class Application extends Controller {
 
 	public static void index() {

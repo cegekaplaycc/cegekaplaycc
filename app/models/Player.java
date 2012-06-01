f81c8156518c43908849168ab87e7e45b0172035
Kaat B <kaat.bogaerts@cegeka.be>
2012-06-01 10:13:22 +0200

    tom, kaat: dashboard: eerste aanzet

--- app/models/Player.java
+++ app/models/Player.java
@@ -56,6 +56,8 @@ public class Player extends Model {
 	public boolean isEmailVerified;
 
 	public String UUID;
+	
+	public int cash = 100;
 
 	public static Player findByUserId(UserId userId) {
 		return find("byUserIdAndProviderType", userId.id, userId.provider)

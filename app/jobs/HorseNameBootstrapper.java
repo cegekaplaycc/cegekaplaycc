a91d3c0ef560aacf59dbd3cc583f4743baa4fbde
Ben Verbeken <ben@ostia.be>
2012-07-10 13:35:14 +0200

    refactoring of HorseNameBootstrapper

--- app/jobs/HorseNameBootstrapper.java
+++ app/jobs/HorseNameBootstrapper.java
@@ -1,40 +1,33 @@
 package jobs;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import models.HorseNamePrefix;
 import models.HorseNameSuffix;
 import play.jobs.Job;
 import play.jobs.OnApplicationStart;
 
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+
 @OnApplicationStart
 public class HorseNameBootstrapper extends Job {
 
-	private static List<String> horsePrefixes;
-	private static List<String> horseSuffixes;
+    private static List<String> horsePrefixes = newArrayList("windy", "shallow",
+            "black", "freaky", "heavy", "amazing", "great", "superfast",
+            "readily", "lazy", "stumbling", "zorro");
+    private static List<String> horseSuffixes = newArrayList("traveller", "night",
+            "shadow", "horse", "rider", "stumper", "humperdink", "legend",
+            "nightmare", "rabbit", "cow", "beast");
 
-	static {
-		horsePrefixes = new ArrayList<String>();
-		horseSuffixes = new ArrayList<String>();
 
-		horsePrefixes.addAll(Arrays.asList(new String[] { "windy", "shallow",
-				"black", "freaky", "heavy", "amazing", "great", "superfast",
-				"readily", "lazy", "stumbling", "zorro" }));
-		horseSuffixes.addAll(Arrays.asList(new String[] { "traveller", "night",
-				"shadow", "horse", "rider", "stumper", "humperdink", "legend",
-				"nightmare", "rabbit", "cow", "beast" }));
-	}
-
-	@Override
-	public void doJob() {
-		for (String prefix : horsePrefixes) {
-			new HorseNamePrefix(prefix).save();
-		}
-		for (String suffix : horseSuffixes) {
-			new HorseNameSuffix(suffix).save();
-		}
-	}
+    @Override
+    public void doJob() {
+        for (String prefix : horsePrefixes) {
+            new HorseNamePrefix(prefix).save();
+        }
+        for (String suffix : horseSuffixes) {
+            new HorseNameSuffix(suffix).save();
+        }
+    }
 
 }

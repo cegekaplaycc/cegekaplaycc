c36bac5f6047f9c4f9fc54ce7e914fa792db76c3
Ben Verbeken <ben@ostia.be>
2012-07-11 13:43:53 +0200

    test fix

--- test/functional/HorseDetailsFunctionalTest.java
+++ test/functional/HorseDetailsFunctionalTest.java
@@ -1,44 +1,44 @@
 package functional;
 
 import litmus.functional.Html;
+import litmus.functional.Request;
 import models.Horse;
 import models.HorseBuilder;
 import models.stock.Food;
-
 import org.junit.Test;
+
+import static models.stock.Food.HAY;
 
 public class HorseDetailsFunctionalTest extends HoldYourHorsesFunctionalTest {
 
-	@Test
-	public void statsAreShown() {
-		Horse horse = new HorseBuilder()
-				.withFitness(12)
-				.withTraining(34)
-				.withFood(Food.HAY)
-				.persist();
+    @Test
+    public void statsAreShown() {
+        Horse horse = new HorseBuilder()
+                .withFitness(12)
+                .withTraining(34)
+                .withFood(HAY)
+                .persist();
 
-		createPlayerBuilder("joske", "vermeulen")
-				.withHorses(horse)
-				.persist();
+        createPlayerBuilder("joske", "vermeulen").withHorses(horse).persist();
 
-		login("joske", "vermeulen");
+        login("joske", "vermeulen");
 
-		Html html = getHtml("/horseDetail?id=" + horse.id);
-		assertThat(html.selectSingle("#fitness")).containsText("12");
-		assertThat(html.selectSingle("#training")).containsText("34");
-	}
+        Html html = getHtml("/horse/" + horse.id);
+        assertThat(html.selectSingle("#fitness")).containsText("12");
+        assertThat(html.selectSingle("#training")).containsText("34");
+    }
 
-	@Test
-	public void foodAndTrainingAreShown() {
-		Horse horse = new HorseBuilder()
-				.withFood(Food.HAY)
-				.persist();
+    @Test
+    public void foodAndTrainingAreShown() {
+        Horse horse = new HorseBuilder()
+                .withFood(HAY)
+                .persist();
 
-		createPlayerBuilder("joske", "vermeulen")
-				.withHorses(horse)
-				.persist();
+        createPlayerBuilder("joske", "vermeulen")
+                .withHorses(horse)
+                .persist();
 
-		Html html = getHtml("/horseDetail?id=" + horse.id);
-		assertThat(html.selectSingle("#food")).containsText("Hay");
-	}
+        Html html = getHtml("/horse/" + horse.id);
+        assertThat(html.selectSingle("#food")).containsText("Hay");
+    }
 }

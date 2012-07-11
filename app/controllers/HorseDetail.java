fc10953149650cf1d63c935b3f14faf709a23064
Kristof Lemmens <kristof.lemmens@cegeka.be>
2012-07-11 16:05:23 +0200

    fixed navigation for horsedetail

--- app/controllers/HorseDetail.java
+++ app/controllers/HorseDetail.java
@@ -9,24 +9,28 @@ import play.data.validation.Required;
 import play.data.validation.Validation;
 import play.i18n.Messages;
 import play.mvc.Controller;
+import play.mvc.With;
 import results.BadRequest;
+import controllers.securesocial.SecureSocial;
 
+@With(SecureSocial.class)
 public class HorseDetail extends Controller {
 
-    public static void changeMoneyForTrainer(@Required Long horseId, @Required int moneyForTrainer) {
-        validate();
-        Horse horse = Horse.<Horse> findById(horseId);
-        horse.moneyForTrainer = moneyForTrainer;
-        horse.save();
-        ok();
-    }
+	public static void changeMoneyForTrainer(@Required Long horseId,
+			@Required int moneyForTrainer) {
+		validate();
+		Horse horse = Horse.<Horse> findById(horseId);
+		horse.moneyForTrainer = moneyForTrainer;
+		horse.save();
+		ok();
+	}
 
 	public static void changeFood(@Required Long id, @Required Food food) {
 		validate();
 		Horse horse = Horse.<Horse> findById(id);
 		horse.food = food;
 		horse.save();
-        renderText(Messages.get("change.food.ok", food));
+		renderText(Messages.get("change.food.ok", food));
 	}
 
 	public static void show(@Required Long id) {

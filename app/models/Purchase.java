0647bb1072484a0862f2560403045f7cc668b06e
Ben Verbeken <ben@ostia.be>
2012-11-19 21:17:19 +0100

    added supplies table to dashboard

--- app/models/Purchase.java
+++ app/models/Purchase.java
@@ -2,15 +2,16 @@ package models;
 
 import models.stock.Food;
 
+import static java.lang.Integer.parseInt;
+
 public class Purchase {
 
-	public Food foodId;
-	public String amount;
-	
-	public int getPrice() {
-		return Integer.parseInt(amount)* foodId.price;
-	}
-	
-	
-	
+    public Food foodId;
+    public String amount;
+
+    public int getPrice() {
+        return parseInt(amount) * foodId.price;
+    }
+
+
 }
